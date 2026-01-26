b5a62aa94f530f8e211578b419fd7ea8be0a19ee
Nick Morozov <neurodeep@gmail.com>
2026-01-26 12:52:59 -0800

    Initial Commit

---
+++ FocusriteVolumeControl/FocusriteVolumeControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusriteVolumeControlApp.swift
+//  FocusriteVolumeControl
+//
+//  Created by Nick Morozov on 2026-01-26.
+//
+
+import SwiftUI
+
+@main
+struct FocusriteVolumeControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
